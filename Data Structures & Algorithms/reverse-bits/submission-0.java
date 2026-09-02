5d1fd6b2cec2ed9f8ccf4a95cc894459d2a87905
Arshiya H <arshiyahamsathali02@gmail.com>
2026-09-02 22:44:43 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseBits(int n) {
+      int result = 0;
+        for (int i = 0; i < 32; i++) { // assuming 32-bit integer
+            result <<= 1;          // shift result left
+            result |= (n & 1);     // add the last bit of n
+            n >>= 1;               // shift n right
+        }
+        return result;  
+        
+    }
+}
